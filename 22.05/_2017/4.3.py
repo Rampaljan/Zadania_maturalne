65f68d6d9cc751b21c7469a91d50ee5caff07b22
Rampaljan <rampaljan2008@gmail.com>
2025-05-22 09:18:05 +0200

    4 final

--- 22.05/_2017/4.3.py
+++ 22.05/_2017/4.3.py
@@ -1,3 +1,11 @@
+def sumacyfr(x):
+    suma = 0
+    for letter in x:
+        suma += int(letter)
+
+    return suma
+
+
 plik = open("22.05\_2017\liczby.txt","r")
 
 ile_rownych_35 = 0
@@ -8,21 +16,28 @@ for line in plik:
     wiersz = line.strip()
     wiersz = wiersz.split(' ')
     
-    a = int(wiersz[0])
-    b = int(wiersz[1])
-    c = int(wiersz[2])
+    a = wiersz[0]
+    b = wiersz[1]
+    c = wiersz[2]
 
-    suma = a + b + c
+    sum_a = sumacyfr(a)
+    sum_b = sumacyfr(b)
+    sum_c = sumacyfr(c)
+
+    
+
+    suma = sum_a + sum_b + sum_c
 
     if suma == 35:
         ile_rownych_35 += 1
 
     if suma > najwieksza_suma:
         najwieksza_suma = suma
+        ile_najwiekszej_sumy = 0
     
     if suma == najwieksza_suma:
         ile_najwiekszej_sumy += 1
 
 
-print(najwieksza_suma, ile_najwiekszej_sumy)
+print(ile_rownych_35, najwieksza_suma, ile_najwiekszej_sumy)
     
